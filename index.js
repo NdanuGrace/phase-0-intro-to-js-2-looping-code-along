f9638ea720e9855c371ea8550ba411b543416b9f
gracekyalo <gracendanu27@gmail.com>
2022-08-11 13:53:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const names=[];
+const events='surprise';
+let thankYoucards=[];
+
+function writeCards(names,events){
+    for(let i=0; i<names.length; i++){
+        let msg=`Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+        thankYoucards.push(msg);
+    }
+console.log(thankYoucards);
+return thankYoucards;
+
+}
+console.log(writeCards(names,events))
+;
+
+ 
+function countDown(number){
+    let countDown=10;
+    while(countDown>=-0){
+        console.log(countDown--);
+    }
+}
+    
